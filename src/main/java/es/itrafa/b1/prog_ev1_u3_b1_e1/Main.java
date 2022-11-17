aa816695a8b89d447128c5b9fcf994c7b141f278
it-rafa <it-rafamartinez@outlook.es>
2022-11-17 04:38:20 +0100

    add package

---
+++ src/main/java/es/itrafa/b1/prog_ev1_u3_b1_e1/Main.java
@@ -0,0 +1,30 @@
+package es.itrafa.b1.prog_ev1_u3_b1_e1;
+/*
+    Ejercicio U3_B1_E1
+    Modifica el ejemplo anterior de forma que exista una nueva variable
+    int y;
+    su valor también se pide por teclado y ocurre:
+    Cuando x vale 10, se imprime (en instrucciones println() diferentes ):
+    ● “x vale 10”
+    ● el resultado de la suma x+y
+
+    Ejemplo:
+    Teclea número X:
+    10
+    Teclea número Y:
+    5
+    x vale 10
+    x +y vale:15
+    cuando x no valga 10 se imprime el mensaje “x NO vale 10”,ejemplo:
+    Teclea número X:
+    4
+    Teclea número Y:
+    6
+    x NO vale 10
+ */
+
+class Init{
+    public static void main(String[] args) {
+
+    }
+}
