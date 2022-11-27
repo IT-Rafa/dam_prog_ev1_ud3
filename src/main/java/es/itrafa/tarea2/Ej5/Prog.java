9262ebc76805d44edcf831f1395aa99a15a6ea69
it-rafa <it-rafamartinez@outlook.es>
2022-11-27 01:28:00 +0100

    progress tarea 2

--- src/main/java/es/itrafa/tarea2/Ej5/Prog.java
+++ src/main/java/es/itrafa/tarea2/Ej5/Prog.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 /**
  * Ejercicio 5
- *
+ * <p>
  * En 1815 nacía Ada Byron, conocida después como Ada Lovelace, destinada a convertirse
  * en la primera programadora de la historia. Amiga de Charles Babbage, siguió con interés
  * los trabajos de éste relacionados con su máquina analítica, que hoy se considera un hito
@@ -13,27 +13,27 @@ import java.util.Scanner;
  * hoy llamaríamos software, mientras que Babbage se preocupaba principalmente por el hardware.
  * Fue Ada quien describió el primer algoritmo pensado específicamente para ser ejecutado por
  * un "ordenador", por lo que se la reconoce como la primera programadora.
- *
+ * <p>
  * El objetivo del algoritmo era el cálculo de los números de Bernouilli.
  * Esta es una secuencia de números racionales que tienen conexiones muy interesantes con teoría
  * de números. Su cálculo es complejo, por lo que a pesar de que han pasado muchos años desde
  * que Ada "programó" cómo obtenerlos, nos conformaremos con uno de sus usos, el cálculo de
  * la fórmula de Faulhaber, o
  * suma de los n primeros números elevados a un valor constante p:
- *
+ * <p>
  * 1p + 2p + 3p + …  + np
- *
+ * <p>
  * 4 3
- *
+ * <p>
  * 1^3 + 2^3 + 3^3 + 4^3 = 100
  * Crea un programa que lea de la entrada estándar múltiples casos de prueba, cada uno en una línea.
  * Un caso de prueba se compondrá de dos números, n y p, entre 1 y 10 ambos incluidos.
- *
+ * <p>
  * El programa finalizará cuando n = p = 0
- *
+ * <p>
  * Por ejemplo:
  * Entrada 	Resultado
- *
+ * <p>
  * 1 1      1
  * 2 2      5
  * 3 3      36
@@ -41,26 +41,33 @@ import java.util.Scanner;
  * 0 0
  */
 public class Prog {
-    public static void main(String[]args){
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int n;
-        int p;
-        do{
-             n = sc.nextInt();
-             p = sc.nextInt();
-             // 4 3
-             // 1^3 + 2^3 + 3^3 + 4^3 = 100
-             int result = 1;
-             System.out.println("1^" + p + " = 1");
-             for (int i = 2; i <= n; i++){
-                 System.out.print( i + "^"+ p + " (");
+        // pedimos primer caso a calcular
+        int n = sc.nextInt();
+        int p = sc.nextInt();
 
-                 int pot = 1;
-                 for (int j = 2; j <= n; j++){
-                     System.out.println( "\t" + i + " * "+ i);
-                 }
-             }
-        }while(n != 0 && p != 0);
+
+        while (n != 0 && p != 0) {
+            int result = 1; // 1^p siempre será 1
+            // (2 hasta n)^p
+            for (int i = 2; i <= n; i++) {
+
+                int pot = 1; // usamos 1 para que de correcto; 0 *... = 0
+                // potencias elevadas a p, menos 1^p
+                for (int j = 0; j < p; j++) {
+                    pot *= i;
+                }
+                // suma potencias
+                result += pot;
+            }
+            // mostramos resultado
+            System.out.println(result);
+
+            // pedimos resto casos
+            n = sc.nextInt();
+            p = sc.nextInt();
+        }
     }
 }
--- src/main/java/es/itrafa/tarea2/Ej6/Prog.java
+++ src/main/java/es/itrafa/tarea2/Ej6/Prog.java
@@ -5,17 +5,56 @@ package es.itrafa.tarea2.Ej6;
  * Crea un método estático denominado diamante(n, c) que dibuje un diamante de la altura n y relleno c, siempre
  * y cuando la altura sea impar y comprendida entre 1 y 33. Un diamante de tamaño 1 es simplemente un caracter c.
  * Para diamantes con altura <=0 o bien altura par simplemente no se imprime nada.
- *
+ * <p>
  * Por ejemplo:
  * diamante(5, '#')
- *
- * 	Resultado
- *
- *   #
- *  ###
+ * <p>
+ * Resultado
+ * <p>
+ * __#
+ * _###
  * #####
- *  ###
- *   #
+ * _###
+ * __#
  */
 public class Prog {
+    public static void main(String[] args) {
+        diamante(5, '#');
+    }
+
+    private static void diamante(int i, char c) {
+        int j;
+        for (j = 0; j < (i / 2); j++) {
+            System.out.print(j + 1 + ":");
+
+            for (int k = 0; k < (i / 2) - j; k++) {
+                System.out.print("_");
+            }
+
+            for (int k = 0; k < j+1; k++) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+
+        System.out.print(j +1 + ":");
+        for (int k = 0; k < i; k++) {
+            System.out.print(c);
+        }
+        System.out.println();
+
+        for (j = j+1; j < i; j++) {
+            System.out.print(j + 1 + ":");
+
+            for (int k = 0; k < j-2; k++) {
+                System.out.print("_");
+            }
+
+            for (int k = 0; k < i - j; k++) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+
+    }
 }
